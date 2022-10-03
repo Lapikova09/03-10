9839caba88065f5ef1469811d81ad86a744ecbab
Lapikova09 <96071466+Lapikova09@users.noreply.github.com>
2022-10-03 18:30:23 +0300

    Add files via upload

---
+++ катерина4.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "russian");
+	//Case1.Дано целое число в диапазоне 1–7.Вывести строку — название дня
+	//	недели, соответствующее данному числу(1 — «понедельник», 2 — «втор -
+	//		ник» и т.д.).
+	//int day;
+	//cin >> day;
+	//if (day == 1) {
+	//	cout << "monday" << endl;
+	//}
+	//int day;
+	//cin >> day;
+	//switch (day) {
+	//case 1:cout << "monday" << endl; break;
+	//case 2:cout << "tuesday" << endl; break;
+	//case 3:cout << "wednesday" << endl; break;
+	//case 4:cout << "thursday" << endl; break;
+	//case 5:cout << "friday" << endl; break;
+	//case 6:cout << "saturday" << endl; break;
+	//case 7:cout << "sunday" << endl; break;
+	//default:cout << "wrong day number" << endl;
+	//}
+	//system("pause");
+
+
+	//int k;
+	//cin >> k;
+	//switch (k) {
+	//case 1:cout << "awful" << endl; break;
+	//case 2:cout << "bad" << endl; break;
+	//case 3:cout << "fine" << endl; break;
+	//case 4:cout << "good" << endl; break;
+	//case 5:cout << "great" << endl; break;
+	//default:cout << "error" << endl;
+//}
+	//system("pause");
+
+
+	//int m;
+	//cin >> m;
+	//switch (m) {
+	//case 1:cout << "winter" << endl; break;
+	//case 2:cout << "winter" << endl; break;
+	//case 12:cout << "winter" << endl; break;
+	//case 4:cout << "spring" << endl; break;
+	//case 5:cout << "spring" << endl; break;
+	//case 3:cout << "spring" << endl; break;
+	//case 7:cout << "summer" << endl; break;
+	//case 8:cout << "summer" << endl; break;
+	//case 6:cout << "summer" << endl; break;
+	//case 10:cout << "autumn" << endl; break;
+	//case 11:cout << "autumn" << endl; break;
+	//case 9:cout << "autumn" << endl; break;
+	//default:cout << "error" << endl;
+	//}
+	//system("pause");
+
+
+
+
+	//int m;
+	//cin >> m;
+	//switch (m) {
+	//case 1:cout << "31" << endl; break;
+	//case 2:cout << "28" << endl; break;
+	//case 12:cout << "31" << endl; break;
+	//case 4:cout << "30" << endl; break;
+	//case 5:cout << "31" << endl; break;
+	//case 3:cout << "31" << endl; break;
+	//case 7:cout << "31" << endl; break;
+	//case 8:cout << "31" << endl; break;
+	//case 6:cout << "30" << endl; break;
+	//case 10:cout << "31" << endl; break;
+	//case 11:cout << "30" << endl; break;
+	//case 9:cout << "30" << endl; break;
+	//default:cout << "error" << endl;
+	//}
+	//system("pause");
+
+
+	//int n;
+	//cin >> n;
+	//int A, B;
+	//B != 0;
+	//cin >> A >> B;
+	//switch (n) {
+	//case 1:cout << A + B << endl; break;
+	//case 2:cout << A - B << endl; break;
+	//case 3:cout << A * B << endl; break;
+	//case 4:cout << A / B << endl; break;
+	//default:cout << "error" << endl;
+	//}
+	//system("pause");
+
+
+	//int a;
+	//cin >> a;
+	//if (a == 0) { cout << "Верно" << endl; }
+	//else{ cout << "Неверно" << endl; }
+	//system("pause");
+
+	//int a;
+	//cin >> a;
+	//if (a > 0) { cout << "Верно" << endl; }
+	//else { cout << "Неверно" << endl; }
+	//system("pause");
+
+   //int a;
+   //cin >> a;
+   //if (a < 0) { cout << "Верно" << endl; }
+   //else { cout << "Неверно" << endl; }
+   //system("pause");
+
+   //int a;
+   //cin >> a;
+   //if (a>0 && a<5)
+   //{
+	//   cout << "Верно" << endl;
+   //}
+  // else { cout << "Неверно" << endl; }
+   //system("pause");
+  
+
+
+   //double a;
+   // cin >> a;
+   //if (a == 0 || a == 2) { cout << a + 7 << endl; }
+   // else { cout << a / 10 << endl; }
+   //system("pause");
+
+
+   //int a, b;
+   // cin >> a >> b;
+   // if(a<=1 && b>=3){ cout << a + b << endl; }
+   //else { cout << a - b << endl; }
+   // system("pause");
+
+
+
+}
